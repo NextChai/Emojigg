472eb6787db544709d00ded74e69d02cbd187a71
NextChai <75498301+NextChai@users.noreply.github.com>
2021-07-28 22:12:07 -0400

    add typehints

--- emojigg/emoji.py
+++ emojigg/emoji.py
@@ -1,22 +1,27 @@
+from typing import (
+    Dict
+)
+
 class Emoji:
     def __init__(
         self, 
         data: dict
         ) -> None:
-        self._raw = data
-        self.id = data.pop('id')
-        self.title = data.pop('title')
-        self.slug = data.pop('slug')
-        self.image = data.pop('image')
-        self.description = data.pop('description')
-        self.license = data.pop('license')
-        self.source = data.pop('source')
-        self.faves = data.pop('faves')
-        self.submitted_by = data.pop('submitted_by')
-        self.width = data.pop('width')
-        self.height = data.pop('height')
-        self.filesize = data.pop('filesize')
-        self.category = data.pop('category')
+        self._raw: Dict = data
+        self.id: int = data.pop('id')
+        self.title: str = data.pop('title')
+        self.slug: str = data.pop('slug')
+        self.image: str = data.pop('image')
+        self.url: str = self.image
+        self.description: str = data.pop('description')
+        self.category: int = data.pop('category')
+        self.license: int = int(data.pop('license'))
+        self.source: str = data.pop('source')
+        self.faves: int = data.pop('faves')
+        self.submitted_by: str = data.pop('submitted_by')
+        self.width: int = data.pop('width')
+        self.height: int = data.pop('height')
+        self.filesize: int = data.pop('filesize')
         
     @property
     def formatted_description(self) -> str:
